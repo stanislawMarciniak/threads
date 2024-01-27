e9903a3f69551542d35900a1982c22628a5d80b4
Stanisław Marciniak <marciniak13ee@gmail.com>
2024-01-27 22:54:28 +0100

    fetching threads

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -4,7 +4,6 @@ import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
-import { ThreadValidation } from "../validations/thread";
 
 interface Params {
   text: string;
@@ -12,6 +11,7 @@ interface Params {
   communityId: string | null;
   path: string;
 }
+
 export async function createThread({
   text,
   author,
@@ -32,4 +32,36 @@ export async function createThread({
   });
 
   revalidatePath(path);
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  //calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  //fetch posts that have no parents (no comments)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
