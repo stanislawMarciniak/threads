368b9ae9d3533500f890154b5809b5c3c44d7875
Stanisław Marciniak <marciniak13ee@gmail.com>
2024-02-02 01:52:10 +0100

    activity page + communities (actions, model, webhook)

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,7 +18,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="flex items-center justify-center w-full min-h-screen">
+            {children}
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
