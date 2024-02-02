ab13892d7a7b928372047eb98bd8408d7e7e8082
Stanisław Marciniak <marciniak.dev@gmail.com>
2024-02-02 16:03:10 +0100

    ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
-    // serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
