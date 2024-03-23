caef8714a4c87941ad487c02d3c6c883dac4e8a3
Stanisław Marciniak <marciniak13ee@gmail.com>
2024-03-23 21:29:09 +0100

    added demo credentials

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,15 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center w-full min-h-screen">
-      <SignUp />;
+    <div className="flex flex-col items-center justify-center min-h-screen text-white">
+      <div>
+        <h2 className="mb-4 text-heading3-bold">Demo account:</h2>
+        <h2 className="mb-2 text-heading4-medium">Username: demo</h2>
+        <h2 className="mb-2 text-heading4-medium">Password: marciniakdemo</h2>
+      </div>
+      <div className="flex justify-center w-full mt-6">
+        <SignUp />
+      </div>
     </div>
   );
 }
